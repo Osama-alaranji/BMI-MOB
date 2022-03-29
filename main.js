d421e645ae04c81ccb664eb937af9657a7d178d1
Osama-alaranji <osamaalaranji@gmail.com>
2022-03-29 08:54:18 -0700

    call elements f

---
+++ main.js
@@ -0,0 +1,7 @@
+let body = document.getElementsByTagName("body");
+let form = document.querySelector("#bmi") ;
+let name = document.querySelector("#name") ;
+let weight = document.querySelector("#weight");
+let height = document.querySelector("#height") ; 
+let male = document.querySelector("#male") ;
+let female = document.querySelector("#female") ;
